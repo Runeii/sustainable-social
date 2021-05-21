b08fb57ea2f50628f9f06901b0678a64722b7862
Andrew Hill <andrew.hill@random.studio>
2021-05-21 15:40:03 +0200

    First attempt at adding stagger logic

--- resizer.js
+++ resizer.js
@@ -1,8 +1,6 @@
 const { clamp, zip } = require('lodash');
 const sharp = require('sharp');
 
-const BACKGROUND_SIZE = 4;
-const STEPS = 5;
 
 const blowOut = async (image, width, finalWidth) => {
 	if (width === finalWidth) {
@@ -48,7 +46,7 @@ const addHighlight = async (original, highlight) => {
 	width = Math.round((fullImageWidth / 100) * width);
 	height = Math.round((fullImageHeight / 100) * height);
 
-	return Array(STEPS).fill(1).map((_, i) => {
+	return Array(5).fill(1).map((_, i) => {
 		const stepIndex = i + 1;
 		let widthAfterGrow = Math.round(width + (width * 4 / stepIndex))
 		let heightAfterGrow = Math.round(height + (height * 4 / stepIndex))
@@ -62,12 +60,60 @@ const addHighlight = async (original, highlight) => {
 	});
 }
 
+const BACKGROUND_SIZE = 4;
+const STEPS = 2;
 
-const resizeImage = async (input, highlights) => {
-	const { width, height } = await input.metadata();
-	const background = await blowOut(input.clone(), BACKGROUND_SIZE, width);
-	console.log(highlights)
-	//background.composite(imagesForCompositing);
+const extractRegion = async (highlight, originalImage) => {
+	const crop = originalImage.clone().extract(highlight);
+
+	const { left, width, top, height } = highlight;
+	return {
+		left,
+		top,
+		input: await crop.toBuffer(),
+	}
+}
+
+const percentageToAbsolute = (imageWidth, imageHeight, { width, height, left, top }) => ({
+	width: Math.floor((width / 100) * imageWidth),
+	height: Math.floor((height / 100) * imageHeight),
+	left: Math.floor((left / 100) * imageWidth),
+	top: Math.floor((top / 100) * imageHeight),
+})
+
+const createStaggeredHighlights = (imageWidth, imageHeight, { width, height, left, top }) => {
+	return Array(STEPS).fill(1).map((_, index) => {
+		const newWidth = width + (imageWidth * ((1 / STEPS) * index));
+		const newHeight = height + (imageHeight * ((1 / STEPS) * index));
+
+		const newLeft = left - ((newWidth - width) / 2);
+		const newTop = top - ((newHeight - height) / 2);
+
+		const boundedLeft = Math.max(newLeft, 0);
+		const boundedTop = Math.max(newTop, 0);
+	
+		const boundedWidth = newWidth + (boundedLeft - newLeft)
+		const boundedHeight = newHeight + (boundedTop - newTop)
+
+		return {
+			index,
+			width: Math.floor(boundedLeft),
+			height: Math.floor(boundedTop),
+			left: Math.floor(boundedWidth),
+			top: Math.floor(boundedHeight),
+		}
+	})
+}
+
+const resizeImage = async (originalImage, highlights) => {
+	const { width, height } = await originalImage.metadata();
+	const background = await blowOut(originalImage.clone(), BACKGROUND_SIZE, width);
+
+	const absoluteHighlights = highlights.map(highlight => percentageToAbsolute(width, height, highlight));
+	const staggeredHighlights = absoluteHighlights.map(highlight => createStaggeredHighlights(width, height, highlight));
+	const extractionRegions = await Promise.all(staggeredHighlights[0].map(async highlight => extractRegion(highlight, originalImage)));
+
+	background.composite(extractionRegions);
 	return background;
 }
 
