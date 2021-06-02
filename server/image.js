77f0c67d70c9ff41cc3c8c32513980b202d36a07
Andrew Hill <andrew.hill@random.studio>
2021-06-02 12:40:43 +0200

    Catch missing image errors

--- server/image.js
+++ server/image.js
@@ -5,13 +5,27 @@ const { UPLOADS_FOLDER } = require('./CONSTANTS.js');
 const generateAnimation = async (res, filename, shapes) => {
 	const filepath = `${UPLOADS_FOLDER}/${filename}`;
 	const original = filepath.split('.').slice(0, filepath.split('.').length - 1).join('.');
-	const input = sharp(original);
+	let input;
+	try {
+		input = sharp(original);
+		// Test is valid image	
+		await input.toBuffer()
+	} catch (error) {
+		return res.send('File has been deleted');
+	}
 	res.status(200);
 	createCompressedAnimation(filepath, input, shapes, res);
 }
 
 const generateStatic = async (res, filename, shapes) => {
-	const input = sharp(`${UPLOADS_FOLDER}/${filename}`);
+	let input;
+	try {
+		input = sharp(`${UPLOADS_FOLDER}/${filename}`);
+		// Test is valid image	
+		await input.toBuffer()
+	} catch (error) {
+		return res.send('File has been deleted');
+	}
 	const image = shapes.length > 0 ? await createCompressedImage(input, shapes) : input;
 	res.status(200);
 	res.contentType('image/jpeg');
@@ -27,17 +41,17 @@ const validateShapes = shapes => shapes.map(({ height, width, left, top }) => {
 
 const generateImage = async (req, res, next) => {
 	const shapes = req.query?.shapes ? JSON.parse(req.query?.shapes) : [];
-	const isAnimation = req.query?.isAnimation ?? false;
-	
+	const isAnimation = (req.query?.isAnimation ?? false) === 'true';
+
 	try {
 		validateShapes(shapes);
-
 		if (isAnimation) {
 			generateAnimation(res, req.params.filename, shapes);
 			return;
 		}
 
 		generateStatic(res, req.params.filename, shapes);
+		return;
 	} catch (error) {
 		next(error)
 	}
